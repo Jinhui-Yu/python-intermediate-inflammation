4d37728d9b2ddea361ad7674a2c3003ed30f5578
Jinhui Yu <164153318+Jinhui-Yu@users.noreply.github.com>
2026-04-15 12:09:41 +0100

    File modified.

--- inflammation/models.py
+++ inflammation/models.py
@@ -32,3 +32,11 @@ def daily_min(data):
     """Calculate the daily min of a 2d inflammation data array."""
     return np.min(data, axis=0)
 
+def patient_normalise(data):
+    """Normalise patient data from a 2D inflammation data array."""
+    if np.any(data < 0):
+        raise ValueError("Inflammation values should not be negative")
+    
+    mean = np.mean(data, axis=0)
+    std = np.std(data, axis=0)
+    return (data - mean) / std
--- tests/test_models.py
+++ tests/test_models.py
@@ -3,7 +3,7 @@
 import numpy as np
 import numpy.testing as npt
 
-from inflammation.models import daily_mean
+from inflammation.models import daily_mean, patient_normalise
 
 def test_daily_mean_zeros():
     """Test that mean function works for an array of zeros."""
@@ -29,3 +29,19 @@ def test_daily_mean_integers():
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(daily_mean(test_input), test_result)
 
+def test_patient_normalise():
+    """Test that normalisation function works for an array of positive integers."""
+
+    test_input = np.array([[1, 2],
+                           [3, 4],
+                           [5, 6]])
+    test_result = np.array([[-1.225, -1.225],
+                            [0, 0],
+                            [1.225, 1.225]])
+
+# x mean=μ: 3, x standard deviation=σ: 1.63299316
+
+
+    # Need to use Numpy testing functions to compare arrays
+    # npt.assert_array_equal(patient_normalise(test_input), test_result)
+    npt.assert_allclose(patient_normalise(test_input), test_result, rtol=1e-3)
